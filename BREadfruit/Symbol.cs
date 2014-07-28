83bfa6c2b016c99f8272eabe2d99a0455d931515
aelena <antonioelena@gmail.com>
2014-07-28 11:31:15 +0200

    IndentLevel added extensive comments

--- BREadfruit/Symbol.cs
+++ BREadfruit/Symbol.cs
@@ -8,12 +8,36 @@ namespace BREadfruit
     public class Symbol
     {
 
+        /// <summary>
+        /// Gets the string representation of the token, for example
+        /// 'Entity' or 'with'.
+        /// </summary>
         public string Token { get; private set; }
+
+        /// <summary>
+        /// Gets the indentation level where the token can live.
+        /// For example, a symbol with IndentLevel = 1 cannot appear 
+        /// at the beginning of a line or at a second level of indentation (2 tabs).
+        /// </summary>
+        public int IndentLevel { get; private set; }
+
+        /// <summary>
+        /// Gets a boolean value that indicates whether this is
+        /// a terminal or nonterminal.
+        /// </summary>
         public bool IsTerminal { get; private set; }
 
-
+        /// <summary>
+        /// Private list of other Symbols that can be considered children
+        /// or siblings of this instance (for the time being, no distinction
+        /// is made between child and sibling).
+        /// </summary>
         private IList<Symbol> _validChildren = new List<Symbol> ();
-        public IEnumerable<Symbol> ValidChildren
+
+        /// <summary>
+        /// Enumerable list of children for callers or validators to consume.
+        /// </summary>
+        public IEnumerable<Symbol> Children
         {
             get
             {
@@ -21,18 +45,46 @@ namespace BREadfruit
             }
         }
 
+        /// <summary>
+        /// Basic constructors. Assumes that the Symbol will be terminal,
+        /// meaning no Children can be added afterwards. If you want to add
+        /// children use the other constructor.
+        /// </summary>
+        /// <param name="symbol">string representation of the token, for example
+        /// 'Entity' or 'with'.
+        /// </param>
         public Symbol ( string symbol )
         {
             this.Token = symbol;
             this.IsTerminal = true;
         }
 
+        /// <summary>
+        /// Constructors that allows the caller to create a symbol and also
+        /// to specify if this will be a terminal or a nonterminal.
+        /// </summary>
+        /// <param name="symbol">string representation of the token, for example
+        /// 'Entity' or 'with'.
+        /// </param>
+        /// <param name="isTerminal">Pass false to create a nonterminal symbol, otherwise
+        /// pass true to create a terminal.</param>
         public Symbol ( string symbol, bool isTerminal)
         {
             this.Token = symbol;
             this.IsTerminal = isTerminal;
         }
 
+        /// <summary>
+        /// Constructors that allows the caller to create a symbol and also
+        /// a list of valid children. By allowing the caller to pass a list of 
+        /// valid children, the Symbol is assumed to be a nonterminal.
+        /// </summary>
+        /// <param name="symbol">string representation of the token, for example
+        /// 'Entity' or 'with'.
+        /// </param>
+        /// <param name="validChildren">
+        /// List of valid children for this Symbol instance.
+        /// </param>
         public Symbol ( string symbol, IList<Symbol> validChildren)
         {
             this.Token = symbol;
@@ -40,6 +92,10 @@ namespace BREadfruit
             this._validChildren = validChildren;
         }
 
+        /// <summary>
+        /// Allows a caller to add child Symbols to a nonterminal Symbol.
+        /// </summary>
+        /// <param name="s"></param>
         public void AddValidChild ( Symbol s )
         {
             if ( this.IsTerminal )
