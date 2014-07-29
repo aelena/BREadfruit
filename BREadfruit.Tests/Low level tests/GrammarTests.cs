93eeaf2cc4ca784c8ae780fc4c632cf570ba1fa6
aelena <antonioelena@gmail.com>
2014-07-29 11:08:27 +0200

    tests for some grammar regeexs

--- BREadfruit.Tests/Low level tests/GrammarTests.cs
+++ BREadfruit.Tests/Low level tests/GrammarTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace BREadfruit.Tests.Low_level_tests
 {
@@ -24,6 +25,27 @@ namespace BREadfruit.Tests.Low_level_tests
         public void ShouldHaveEntityRegex ()
         {
             Assert.That ( Grammar.EntityLineRegex.Contains ( "Dynamic" ) );
+        }
+
+
+        [TestCase ( Grammar.PositiveIntegerRegex, 0, Result = true )]
+        [TestCase ( Grammar.PositiveIntegerRegex, 24, Result = true )]
+        [TestCase ( Grammar.PositiveIntegerRegex, -24, Result = false )]
+        [TestCase ( Grammar.PositiveIntegerRegex, -1.0, Result = false )]
+        [TestCase ( Grammar.PositiveIntegerRegex, 'a', Result = false )]
+        [TestCase ( Grammar.IntegerRegex, 0, Result = true )]
+        [TestCase ( Grammar.IntegerRegex, 288, Result = true )]
+        [TestCase ( Grammar.IntegerRegex, -288, Result = true )]
+        [TestCase ( Grammar.IntegerRegex, 2.88, Result = false )]
+        [TestCase ( Grammar.IntegerRegex, -2.88, Result = false )]
+        [TestCase ( Grammar.FloatRegex, -2.88d, Result = true )]
+        [TestCase ( Grammar.FloatRegex, 2.88d, Result = true )]
+        [TestCase ( Grammar.FloatRegex, 288d, Result = true )]
+        [TestCase ( Grammar.FloatRegex, -0.001d, Result = true )]
+        [TestCase ( Grammar.FloatRegex, 0.0001d, Result = true )]
+        public bool GrammarRegexsShouldWork(string regex, ValueType value)
+        {
+            return Regex.IsMatch ( value.ToString(), regex );
         }
 
     }
