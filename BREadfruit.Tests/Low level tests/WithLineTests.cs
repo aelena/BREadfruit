f617ac029b47595284469447d9b9a0baffe1fc27
aelena <antonioelena@gmail.com>
2014-07-29 16:32:15 +0200

    some tests

--- BREadfruit.Tests/Low level tests/WithLineTests.cs
+++ BREadfruit.Tests/Low level tests/WithLineTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace BREadfruit.Tests.Low_level_tests
 {
@@ -11,5 +12,23 @@ namespace BREadfruit.Tests.Low_level_tests
     public class WithLineTests
     {
 
+
+        [TestCase ( "\twith defaults", 1, Result = true )]
+        [TestCase ( "\twith triggers", 1, Result = true )]
+        [TestCase ( "\twith rules", 1, Result = true )]
+        [TestCase ( "\twith constraints", 1, Result = true )]
+        /*
+         * even when passing 0 as the indent level for this symbol, which 
+         * is wrong according to the grammar, this test passes as the
+         * indentLevel is recalculated on constructing the LineInfo instance.
+         */
+        [TestCase ( "\twith constraints", 0, Result = true )]
+        [TestCase ( "with constraints", 1, Result = false )]
+        [TestCase ( "\twith booz", 1, Result = false )]
+        public bool ValidateWithLineInfosCorrectly ( string representation, int indentLevel )
+        {
+            var li = new LineInfo ( representation );
+            return LineParser.IsAValidSentence ( li );
+        }
     }
 }
