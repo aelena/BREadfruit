f6a36bfd1849da07d10e4f0fa5d502e96455391b
aelena <antonioelena@gmail.com>
2014-08-04 12:42:33 +0200

    prior to deletion

---
+++ BREadfruit/Conditions/AliasedToken.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BREadfruit.Conditions
+{
+    public abstract class AliasedToken
+    {
+        protected string _name;
+        public string Identifier
+        {
+            get { return _name; }
+        }
+
+
+        // ---------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// Stores other syntax that might be available for this same operator.
+        /// For example, we can accept 'bigger or equals than' or '>=' as well 
+        /// within the valid file syntax
+        /// </summary>
+        protected List<String> _aliases;
+        public IEnumerable<String> Aliases
+        {
+            get
+            {
+                return this._aliases;
+            }
+        }
+
+
+        /// <summary>
+        /// Clients call pass a specified token ( any arbitrary string actually )
+        /// and get a value that indicates whether that token is an valid and +
+        /// known alias for this Operator.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public bool MatchesToken ( string token )
+        {
+            if ( !String.IsNullOrWhiteSpace ( token ) )
+            {
+                if ( this._aliases != null )
+                {
+                    var matched = from x in _aliases
+                                  where x == token
+                                  select x;
+
+                    return ( matched != null && matched.Count () > 0 );
+                }
+            }
+            return false;
+
+        }
+
+
+        // ---------------------------------------------------------------------------------
+
+    }
+}
--- BREadfruit/Conditions/Operator.cs
+++ BREadfruit/Conditions/Operator.cs
@@ -6,39 +6,15 @@ using System.Threading.Tasks;
 
 namespace BREadfruit.Conditions
 {
-    public class Operator
+    public class Operator : AliasedToken
     {
-
-        private readonly string _name;
-        public string Identifier
-        {
-            get { return _name; }
-        }
-
-
-        // ---------------------------------------------------------------------------------
-
-
-        /// <summary>
-        /// Stores other syntax that might be available for this same operator.
-        /// For example, we can accept 'bigger or equals than' or '>=' as well 
-        /// within the valid file syntax
-        /// </summary>
-        private List<String> _aliases;
-        public IEnumerable<String> Aliases
-        {
-            get
-            {
-                return this._aliases;
-            }
-        }
 
 
         public Operator ( string identifier, IEnumerable<string> aliases = null )
         {
             if ( String.IsNullOrWhiteSpace ( identifier ) )
-                throw new ArgumentNullException ( "identifier" );
-            
+                throw new ArgumentNullException ( "identifier", "the identifier for an Operator instance cannot be null" );
+
             this._name = identifier;
             this._aliases = new List<string> ();
             this._aliases.Add ( identifier );
@@ -51,32 +27,6 @@ namespace BREadfruit.Conditions
         // ---------------------------------------------------------------------------------
 
 
-        /// <summary>
-        /// Clients call pass a specified token ( any arbitrary string actually )
-        /// and get a value that indicates whether that token is an valid and +
-        /// known alias for this Operator.
-        /// </summary>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        public bool MatchesToken ( string token )
-        {
-            if ( !String.IsNullOrWhiteSpace ( token ) )
-            {
-                if ( this._aliases != null )
-                {
-                    var matched = from x in _aliases
-                                  where x == token
-                                  select x;
-
-                    return ( matched != null && matched.Count () > 0 );
-                }
-            }
-            return false;
-
-        }
-
-
-        // ---------------------------------------------------------------------------------
-
+       
     }
 }
