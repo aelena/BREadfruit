7a1b4c4a9b437e26fe2fc18946947c47f479dae6
aelena <antonioelena@gmail.com>
2014-08-04 17:01:41 +0200

    do not add main token to alia

---
+++ BREadfruit/Conditions/UnaryAction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BREadfruit.Conditions
+{
+    public class UnaryAction : AliasedSymbol
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        public UnaryAction ( Symbol s ) :
+            base ( s.Token, s.IndentLevel, s.IsTerminal )
+        {
+            if ( s.Children != null )
+                foreach ( var c in s.Children )
+                    base.AddValidChild ( c );
+        }
+
+
+        // ---------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="identifier"></param>
+        /// <param name="indentLevel"></param>
+        /// <param name="isTerminal"></param>
+        /// <param name="aliases"></param>
+        public UnaryAction ( string identifier, int indentLevel, bool isTerminal, IEnumerable<string> aliases = null )
+            : base ( identifier, indentLevel, isTerminal )
+        {
+            if ( String.IsNullOrWhiteSpace ( identifier ) )
+                throw new ArgumentNullException ( "identifier", "The identifier for a ResultAction instance cannot be null" );
+
+            this.Token = identifier;
+
+            if ( aliases != null )
+                this._aliases.AddRange ( aliases );
+
+        }
+
+
+        // ---------------------------------------------------------------------------------
+
+    }
+}
