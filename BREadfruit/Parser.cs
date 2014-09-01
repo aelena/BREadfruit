3ed5b6eb4afe08067831220687332aa7c1941436
aelena <antonioelena@gmail.com>
2014-09-01 16:06:39 +0200

    change in parsing of trigger sections

--- BREadfruit/Parser.cs
+++ BREadfruit/Parser.cs
@@ -215,7 +215,10 @@ namespace BREadfruit
                             {
                                 if ( lineInfo.Tokens.Count () == 2 )
                                 {
-                                    var _trigger = new Trigger ( lineInfo.Tokens.First ().Token, lineInfo.Tokens.ElementAt ( 1 ).Token );
+                                    var _firstToken = lineInfo.Tokens.First ().Token;
+                                    if ( lineInfo.Tokens.First ().Token.Trim ().StartsWith ( "this." ) )
+                                        _firstToken = _firstToken.Replace ( "this", this._entities.Last ().Name );
+                                    var _trigger = new Trigger ( _firstToken, lineInfo.Tokens.ElementAt ( 1 ).Token );
                                     this._entities.Last ().AddTrigger ( _trigger );
                                 }
                             }
@@ -234,6 +237,7 @@ namespace BREadfruit
                 }   // close of while loop
 
             }
+
 
         }
 
