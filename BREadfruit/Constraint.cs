770642cfc90c053dd6aeb72c67d2ca4336bb01fe
aelena <antonioelena@gmail.com>
2014-09-02 09:37:40 +0200

    new test files + 2 untracked files

---
+++ BREadfruit/Constraint.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BREadfruit
+{
+    public class Constraint
+    {
+        private readonly string _constraintName;
+        /// <summary>
+        /// Gets an naming descriptor that indicates the kind 
+        /// of constraint this represents
+        /// </summary>
+        public string Name
+        {
+            get { return _constraintName; }
+        }
+
+        // ---------------------------------------------------------------------------------
+
+        private readonly string _regex;
+
+        /// <summary>
+        /// Gets the regex that was set up during construction.
+        /// Not all constraints might have regular expressions and therefore this
+        /// property could return null.
+        /// </summary>
+        public string RegexValidationPattern
+        {
+            get { return _regex; }
+        }
+
+        // ---------------------------------------------------------------------------------
+
+
+        public Constraint ( string name, string regexValidationPattern = null )
+        {
+            this._constraintName = name;
+            this._regex = regexValidationPattern;
+        }
+
+        // ---------------------------------------------------------------------------------
+
+
+        public override string ToString ()
+        {
+            if ( RegexValidationPattern != null )
+                return String.Format ( "{0} {1}", this.Name, this.RegexValidationPattern );
+            return this.Name;
+        }
+
+        // ---------------------------------------------------------------------------------
+
+    }
+}
---
+++ BREadfruit/Trigger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BREadfruit
+{
+    public class Trigger
+    {
+        private readonly string _triggerName;
+        /// <summary>
+        /// Gets an event descriptor that indicates the event 
+        /// that this trigger represents
+        /// </summary>
+        public string Event
+        {
+            get { return _triggerName; }
+        }
+
+        // ---------------------------------------------------------------------------------
+
+
+        private readonly string _elementName;
+        /// <summary>
+        /// Gets an event descriptor that indicates the element 
+        /// on which the event is declared
+        /// </summary>
+        public string Target
+        {
+            get { return _elementName; }
+        }
+
+        // ---------------------------------------------------------------------------------
+
+
+        public Trigger ( string elementName, string eventName )
+        {
+            this._elementName = elementName;
+            this._triggerName = eventName;
+        }
+
+        // ---------------------------------------------------------------------------------
+
+
+        public override string ToString ()
+        {
+            return String.Format ( "{0} {1}", this.Target, this.Event );
+        }
+
+        // ---------------------------------------------------------------------------------
+
+    }
+}
