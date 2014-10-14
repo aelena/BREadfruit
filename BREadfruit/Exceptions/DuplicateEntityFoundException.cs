a5ede7fa31ab317d3d18d66c4e8c3e6428f63483
aelena <antonioelena@gmail.com>
2014-10-14 13:14:18 +0200

    DuplicateEntityFoundException added

---
+++ BREadfruit/Exceptions/DuplicateEntityFoundException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BREadfruit.Exceptions
+{
+    public class DuplicateEntityFoundException : Exception, ISerializable
+    {
+
+
+        public DuplicateEntityFoundException ()
+            : base ( Grammar.DuplicateEntityFoundExceptionDefaultMessage )
+        {
+        }
+
+        public DuplicateEntityFoundException ( string message )
+            : base ( message )
+        {
+        }
+
+        public DuplicateEntityFoundException ( string message, Exception inner )
+            : base ( message, inner )
+        {
+        }
+
+        protected DuplicateEntityFoundException ( SerializationInfo info, StreamingContext context )
+            : base ( info, context )
+        {
+        }
+    }
+}
